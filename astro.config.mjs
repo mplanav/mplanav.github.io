18c95053c54010682ecb6b01207ce2195d1ae43f
mplanav <marc.plana.villalbi@estudiantat.upc.edu>
2025-05-24 13:12:13 +0200

    Prep for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    outDir: 'docs',
-    base: '/marc-plana/',  // Reemplaza 'marc-plana' por el nombre exacto de tu repo
+  base: '/portfolio/',
 });
